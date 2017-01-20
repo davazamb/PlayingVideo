c5ad87ded8a873e19f3c3645234d8cf87f395d10
David Zambrano <davazamb10@outlook.com>
2017-01-20 18:23:44 -0400

    Incluidas Carpetas Videofile y Custom y controlador

---
+++ PlayingVideo/CustomResult/VideoResult.cs
@@ -0,0 +1,29 @@
+﻿using System.IO; 
+using System.Web.Hosting; 
+using System.Web.Mvc;
+namespace PlayingVideo.CustomResult
+{
+    public class VideoResult : ActionResult
+    {
+        /// <summary> 
+        /// The below method will respond with the Video file 
+        /// </summary> 
+        /// <param name="context"></param> 
+        public override void ExecuteResult(ControllerContext context)
+        {
+            //The File Path 
+            var videoFilePath = HostingEnvironment.MapPath("~/VideoFile/CumplePedro.mp4");
+            //The header information 
+            context.HttpContext.Response.AddHeader("Content-Disposition", "attachment; filename=Win8.mp4");
+            var file = new FileInfo(videoFilePath);
+            //Check the file exist,  it will be written into the response 
+            if (file.Exists)
+            {
+                var stream = file.OpenRead();
+                var bytesinfile = new byte[stream.Length];
+                stream.Read(bytesinfile, 0, (int)file.Length);
+                context.HttpContext.Response.BinaryWrite(bytesinfile);
+            }
+        }
+    }
+}
